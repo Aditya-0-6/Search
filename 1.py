733d68306ca29b0ce7af30c076bd146cebe251d1
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2022-10-26 12:05:18 +0530

    Added updates to the prime number python file

--- 1.py
+++ 1.py
@@ -1,14 +1,14 @@
 num = 10
 if num > 1:
-	
-	for i in range(3, int(num/2)+1):
-		if (num % i+1) == 0:
-			print(num, "is not a prime number")
-			break
-		else:
-			print(num, "is a prime number")
+
+    for i in range(3, int(num / 2) + 1):
+        if (num % i + 1) == 0:
+            print(num, "is not a prime number")
+            break
+        else:
+            print(num, "is a prime number")
+
+elif num == 1:
+    print(num, "is neither composite nor prime number.")
 else:
-	print(num, "is not a prime number")
-
-
-
+    print(num, "is not a prime number")
